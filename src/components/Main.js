2d3e52e38b09efb5b16232ef5258e20611292b28
pooli0911 <mua0911@gmail.com>
2021-04-12 23:20:22 +0800

    carousel

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,33 @@
-import Main from "../img/home_img/main.png"
+import Main from "../img/home_img/main.png";
+import { Carousel } from "antd";
 
-
-
-export default function Top(){
-    return(
-        <div className="main">
-            <img className="main-img" src={Main}></img>
+export default function Top() {
+    const contentStyle = {
+        height: '160px',
+        color: '#fff',
+        lineHeight: '160px',
+        textAlign: 'center',
+        background: '#41439e',
+    };
+    return (
+        <div>
+            <div className="main">
+                <img className="main-img" src={Main}></img>
+            </div>
+            <Carousel autoplay>
+                <div>
+                    <h3 style={contentStyle}>1</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>2</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>3</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>4</h3>
+                </div>
+            </Carousel>
         </div>
     );
 }
