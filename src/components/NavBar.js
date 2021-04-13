170c24750ec0997496ef1e96c6ba8a43c85bd9e0
pooli0911 <mua0911@gmail.com>
2021-04-13 11:16:34 +0800

    circle

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,25 @@
 import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
-    return(
+    return (
         <div className="nav-bar">
-            <div className="circle">
-            <NavLink to="/battle" className="nav-item" activeClassName="nav-item--active">
+            <NavLink to="/battle" className="circle nav-item" activeClassName="nav-item--active">
                 戰鬥
             </NavLink>
-            </div>
-            <div className="circle">
-            <NavLink to="/kingdom" className="nav-item" activeClassName="nav-item--active">
+            <NavLink to="/kingdom" className="circle nav-item" activeClassName="nav-item--active">
                 王國
             </NavLink>
-            </div>
-            <div className="circle">
-            <NavLink to="/adventure" className="nav-item" activeClassName="nav-item--active">
+            <NavLink to="/adventure" className="circle nav-item" activeClassName="nav-item--active">
                 冒險
             </NavLink>
-            </div>
-            <div className="circle">
-            <NavLink to="/cookie" className="nav-item" activeClassName="nav-item--active">
+            <NavLink to="/cookie" className="circle nav-item" activeClassName="nav-item--active">
                 餅乾
             </NavLink>
-            </div>
-            <div className="circle">
-            <NavLink to="/pvp" className="nav-item" activeClassName="nav-item--active">
+            <NavLink to="/pvp" className="circle nav-item" activeClassName="nav-item--active">
                 PVP
             </NavLink>
-            </div>
-            
+
+
         </div>
     );
 }
