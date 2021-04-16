00fcf712082055a6aed6966c1c01f40ea5ec9e15
pooli0911 <mua0911@gmail.com>
2021-04-16 16:03:26 +0800

    cookie-detail

--- src/components/CookieDetail.js
+++ src/components/CookieDetail.js
@@ -1,40 +1,42 @@
 import { useState } from "react";
 import { Row, Col } from "antd";
 
-function CookieDetail({cookies}) {
-    return(
-        <div>
-        {cookies.map(cookie=>(
-            <Row gutter={[32, 32]}>
-            <Col
-            lg={{span:8,offset:2}}>
-                <img alt=""
-                className="cookie-image"
-                src={cookie.image}/>
-            </Col>
-            <Col lg={{span:12}}>
-                <div className="detail-box">
-                    <h1 className="cookie-name">
-                        {cookie.name}
-                    </h1>
-                    <p className="cookie-state">
-                        站位：{cookie.state}
-                    </p>
-                    <p className="cookie-description">
-                        技能：{cookie.description}
-                    </p>
-                    <p className="cookie-comment">
-                        PVE/PVP 評價：{cookie.comment}
-                    </p>
-                    <p className="cookie-story">
-                        餅乾故事：{cookie.description_long}
-                    </p>
-                </div>
-            </Col>
-        </Row>
-        ))}
+function CookieDetail({ cookies }) {
+    return (
+        <div className="cookie-flex">
+            {cookies.map(cookie => (
+                <Row gutter={[32, 32]}>
+                    <div className="ckies">
+                        <Col
+                            lg={{ span: 6, offset: 3 }}>
+                            <img alt=""
+                                className="cookie-image"
+                                src={cookie.image} />
+                        </Col>
+                        <Col lg={{ span: 8 }}>
+                            <div className="detail-box">
+                                <h1 className="cookie-name">
+                                    {cookie.name}
+                                </h1>
+                                <p className="cookie-state">
+                                    站位：{cookie.state}
+                                </p>
+                                <p className="cookie-description">
+                                    技能：{cookie.description}
+                                </p>
+                                <p className="cookie-comment">
+                                    PVE/PVP 評價：{cookie.comment}
+                                </p>
+                                <p className="cookie-story">
+                                    餅乾故事：{cookie.description_long}
+                                </p>
+                            </div>
+                        </Col>
+                    </div>
+                </Row>
+            ))}
         </div>
-        
+
     );
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createContext, useReducer } from "react";
 import useReducerWithThunk from 'use-reducer-thunk';
-import { SET_HOME_IMAGE, SET_NAVBAR_ACTIVEITEM,SET_COOKIE_IMAGE } from "../utils/constants";
+import { SET_HOME_IMAGE, SET_NAVBAR_ACTIVEITEM, SET_COOKIE_IMAGE } from "../utils/constants";
 import battle from "../json/home_img1.json"
 import tank from "../json/tank.json"
 
@@ -8,33 +8,31 @@ import tank from "../json/tank.json"
 
 export const StoreContext = createContext();
 const initialState = {
-   home_img: {
-      battle,
-   },
+   home_img: battle,
    navBar: {
-      activeItem:"/"
+      activeItem: "/"
    },
    cookie: tank,
-   
+
 }
 
-function reducer(state, action){
+function reducer(state, action) {
    console.log(action)
-   switch(action.type){
+   switch (action.type) {
       case SET_HOME_IMAGE:
-         return{
-            ...state, home_img:action.payload,
+         return {
+            ...state, home_img: action.payload,
          };
       case SET_NAVBAR_ACTIVEITEM:
-         return{
+         return {
             ...state,
-            navBar:{
+            navBar: {
                activeItem: action.payload,
             }
          }
       case SET_COOKIE_IMAGE:
-         return{
-            ...state,cookie:action.payload,
+         return {
+            ...state, cookie: action.payload,
          }
       default:
          return state;
@@ -43,14 +41,13 @@ function reducer(state, action){
 
 
 export function StoreProvider(props) {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const value = { state, dispatch };
- 
-    return (
-       <StoreContext.Provider value={value}>
-          {props.children}
-       </StoreContext.Provider>
-    );
- }
+   const [state, dispatch] = useReducer(reducer, initialState);
+   const value = { state, dispatch };
 
- 
+   return (
+      <StoreContext.Provider value={value}>
+         {props.children}
+      </StoreContext.Provider>
+   );
+}
+
