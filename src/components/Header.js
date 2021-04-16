6a53cf226d08ce4e86dccb290e35cf6708661f5a
pooli0911 <mua0911@gmail.com>
2021-04-16 14:47:25 +0800

    backtop

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,26 @@
 import { useContext } from "react";
+import { BackTop } from 'antd';
 import { Link } from "react-router-dom";
 import Logo from "../img/header/logo.png"
 
 
-export default function Header(){
-    return(
-<div className="header">
-    <header className="header-wrap">
-        <div className="header-logo">
-            <Link to ="/">
-                <img className="img-logo" src={Logo}></img>
-            </Link>
-        </div>
-        <div className="header-text">
-            <Link to className="header-text-item">遊戲介紹</Link>
-            <Link to ="/cookie" className="header-text-item">餅乾介紹</Link>
-            <Link to className="header-text-item">陣容預覽</Link>
-        </div>
+export default function Header() {
+    return (
+        <div className="header">
+            <header className="header-wrap">
+                <div className="header-logo">
+                    <Link to="/">
+                        <img className="img-logo" src={Logo}></img>
+                    </Link>
+                </div>
+                <div className="header-text">
+                    <Link to className="header-text-item">遊戲介紹</Link>
+                    <Link to="/cookie" className="header-text-item">餅乾介紹</Link>
+                    <Link to className="header-text-item">陣容預覽</Link>
+                </div>
 
-    </header>
-
-</div>
+            </header>
+            <BackTop />
+        </div>
     );
 }
