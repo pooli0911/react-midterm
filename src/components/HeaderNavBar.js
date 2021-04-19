c23d8dbe6eae88af4c55b71823eb9c9eacb12636
pooli0911 <mua0911@gmail.com>
2021-04-19 12:46:33 +0800

    linkto

--- src/components/HeaderNavBar.js
+++ src/components/HeaderNavBar.js
@@ -6,7 +6,7 @@ export default function HeaderNavBar({ isOnTouch }) {
             <div className={`header-text ${isOnTouch ? "" : "header-text-wrap"}`}>
                 <Link to="" className="header-text-item" activeclassName="header-text-item--active">遊戲介紹</Link>
                 <Link to="/cookie" className="header-text-item" activeclassName="header-text-item--active">餅乾介紹</Link>
-                <Link to="" className="header-text-item" activeclassName="header-text-item--active">陣容預覽</Link>
+                <Link to="/team" className="header-text-item" activeclassName="header-text-item--active">陣容預覽</Link>
             </div>
 
         </>
