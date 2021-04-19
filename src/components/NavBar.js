9f62c0abec9eb05d886edcda49fbb013708a780a
pooli0911 <mua0911@gmail.com>
2021-04-19 14:13:42 +0800

    home-rwd

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,21 +3,25 @@ import NavItem from "./NavItem";
 export default function NavBar() {
     return (
         <div className="nav-bar">
-            <NavItem to="/home/battle" className="circle nav-item" activeClassName="nav-item--active">
-                戰鬥
+            <div className="first">
+                <NavItem to="/home/battle" className="circle nav-item" activeClassName="nav-item--active">
+                    戰鬥
             </NavItem>
-            <NavItem to="/home/kingdom" className="circle nav-item" activeClassName="nav-item--active">
-                王國
+                <NavItem to="/home/kingdom" className="circle nav-item" activeClassName="nav-item--active">
+                    王國
             </NavItem>
-            <NavItem to="/home/adventure" className="circle nav-item" activeClassName="nav-item--active">
-                冒險
+                <NavItem to="/home/adventure" className="circle nav-item" activeClassName="nav-item--active">
+                    冒險
             </NavItem>
-            <NavItem to="/home/cookie" className="circle nav-item" activeClassName="nav-item--active">
-                餅乾
+            </div>
+            <div className="second">
+                <NavItem to="/home/cookie" className="circle nav-item" activeClassName="nav-item--active">
+                    餅乾
             </NavItem>
-            <NavItem to="/home/pvp" className="circle nav-item" activeClassName="nav-item--active">
-                PVP
+                <NavItem to="/home/pvp" className="circle nav-item" activeClassName="nav-item--active">
+                    PVP
             </NavItem>
+            </div>
 
 
         </div>
