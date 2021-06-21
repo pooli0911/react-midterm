99a44d0419d9459b21237372f1740d7a1542153c
pooli0911 <mua0911@gmail.com>
2021-06-21 19:16:31 +0800

    fade-left

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
+import { useEffect } from 'react';
 import issueImg1 from "../img/home_img/issue.png"
 import issueImg2 from "../img/home_img/issue2.png"
 import issueImg3 from "../img/home_img/issue3.png"
 import issueImg4 from "../img/home_img/issue4.png"
 import NavBar from "./NavBar"
+import Aos from "aos";
+
 
 export default function Content() {
+    useEffect(() => {
+        Aos.init({ duration: 1000 });
+    }, [])
     return (
         <div className="main-content">
             <div className="hot-issue">
@@ -13,12 +19,14 @@ export default function Content() {
                     <p className="hot-issue">HOT ISSUE</p>
                 </div>
                 <Link className="more">MORE +</Link>
+
                 <div className="hot-issue-img">
-                    <Link><img className="issue-img" src={issueImg1}></img></Link>
-                    <Link><img className="issue-img" src={issueImg2}></img></Link>
-                    <Link><img className="issue-img" src={issueImg3}></img></Link>
-                    <Link><img className="issue-img" src={issueImg4}></img></Link>
+                    <Link><img data-aos="fade-left" className="issue-img" src={issueImg1}></img></Link>
+                    <Link><img data-aos="fade-left" className="issue-img" src={issueImg2}></img></Link>
+                    <Link><img data-aos="fade-left" className="issue-img" src={issueImg3}></img></Link>
+                    <Link><img data-aos="fade-left" className="issue-img" src={issueImg4}></img></Link>
                 </div>
+
                 <Link className="smallmore">MORE +</Link>
             </div>
             <div className="game-overview">
@@ -27,7 +35,7 @@ export default function Content() {
                 </div>
                 <NavBar />
             </div>
-        </div>
+        </div >
     );
 
 }
